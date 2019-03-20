b15acfa4246c6b7b955ba1d72b2ecb810d1d2829
Jessica Pena <penajessica123@yahoo.com>
2019-03-19 20:21:59 -0500

    checkpoint 1

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
@@ -19,30 +20,61 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+function movePiece(begin, end) { 
+ stacks[end].push(stacks[begin].pop())
 
 }
 
-function isLegal() {
+function isLegal(start, end) {
+ 
+  let index = stacks[start].length - 1
+  let indexTwo = stacks[end].length - 1
+  if(stacks[start][index] < stacks[end][indexTwo] || stacks[end][indexTwo] === undefined){
+    return true
+  } else {
+    return false
+  }
   // Your code here
+  // if((startStack === 'a'|| startStack === 'b' || startStack === 'c') && (endStack === 'a'||endStack === 'b' || endStack === 'c')
+  // ){
+
+  //   if(stacks[startStack][stacks[startStack].length -1] > stacks[endStack][stacks[endStack].length - 1]){
+  //     return true
+  //   } else {
+  //     return false
+  //   } }
+  //   else   {
+  //     console.log( 'incorrect input')
+  //     return false
+    
+  // }
 
 }
 
 function checkForWin() {
   // Your code here
-
+  if(stacks["b"].length === 4 || stacks['c'].length === 4){
+    console.log('you win')
+    return true
+  } else {
+    return false
+  }
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+  console.log(`my startStack is ${startStack}`)
+  console.log(`endStack is ${endStack}`)
+  console.log("testing")
+  if(isLegal(startStack, endStack)){
+    movePiece(startStack, endStack)
+  }
 }
 
 function getPrompt() {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
+      console.log("get prompt")
       towersOfHanoi(startStack, endStack);
       getPrompt();
     });
