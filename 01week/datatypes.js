cd7478606af5d7f7b7a7ee8ae1de2e39c9eba884
Jessica Pena <penajessica123@yahoo.com>
2019-06-21 08:54:45 -0700

    turning in the assignment did not create anything in the DOM

---
+++ 01week/datatypes.js
@@ -0,0 +1,65 @@
+ // Write a JavaScript program to display the current day and time.
+    new Date().toLocaleString("en-US" , {timeZone: "America/Chicago"})
+
+// Write a JavaScript program to convert a number to a string.
+  var num = 15;
+  var num1 = num.toString();
+  console.log(num1);
+
+
+// Write a JavaScript program to convert a string to the number.
+  Number('22');
+
+// Write a JavaScript program that takes in different data-types and prints out whether they are a:
+
+// Boolean 
+  var thisIsBoolean = true;
+  console.log(typeof(thisIsBoolean));
+
+// Null
+  var iDidNotGiveThisVarValue = null;
+  console.log(iDidNotGiveThisVarValue);
+
+// Undefined
+  var j 
+  console.log(j)
+
+// Number
+  var nums = 7;
+  console.log(typeof(nums));
+
+// NaN
+  var num = "apple"
+  var num1 = 4;
+  console.log(num * num1);
+
+// String
+  var myString = "Jess";
+  console.log(typeof(myString));
+
+// Write a JavaScript program that adds 2 numbers together.
+
+  let num1 = 2
+  let num2 = 3
+  let total = num1 + num2
+  console.log(total);
+
+// Write a JavaScript program that runs only when 2 things are true.
+let x = 7;
+let y = 6;
+function truthy(x , y){
+if(x > 5 && y > 4)
+return true;
+}
+// Write a JavaScript program that runs when 1 of 2 things are true.
+let x=2;
+let y=3;
+if(x < 5 || y > 4) {
+
+}
+// Write a JavaScript program that runs when both things are not true.
+let x=2;
+let y=6;
+if(x != < 5 && y != > 4) {
+    return false;
+ }
